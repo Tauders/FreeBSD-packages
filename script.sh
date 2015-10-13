c111db2cc7cb54f7d67228a81d5c2a9b2aa25aed
Vladimir Asmolovsky <tauders@gmail.com>
2015-10-13 23:57:29 +0600

    Добавил немного конфигов, изменил скрипт.

--- script.sh
+++ script.sh
@@ -5,5 +5,42 @@ pkg update
 echo '----------------------------------'
 echo 'Install packages'
 echo '----------------------------------'
-pkg install -y mc qpopper ntp mutt apache24 samba42 xorg wireshark opera isc-dhcp43-server bind910
+pkg install -y mc qpopper ntp mutt apache24 samba42 x11/kde4 wireshark opera isc-dhcp43-server bind910
+echo '----------------------------------'
+echo 'Generate BIND "secret" key'
+echo '----------------------------------'
+rndc-confgen -a
+echo '----------------------------------'
+echo 'Change rc.conf'
+echo '----------------------------------'
+cat > /etc/rc.conf << EOF
+hostname=""
+firewall_enable="YES"
+firewall_type="open"
+firewall_logging="NO" 
+gateway_enable="YES"
+named_enable="NO"
+natd_enable="NO"
+inetd_enable="NO"
+sendmail_enable="NO"
+sendmail_submit_enable="NO"
+sendmail_outbound_enable="NO"
+sendmail_msp_queue_enable="NO"
+sshd_enable="YES"
+rpcbind_enable="NO"
+nfs_client_enable="NO"
+nfs_server_enable="NO"
+rpc_lockd_enable="NO"
+rpc_statd_enable="NO"
+cron_enable="YES"
+fusefs_enable="YES"
+arpwatch_enable="NO"
+diskprep_enable="NO"
+dbus_enable="YES"
+hald_enable="YES"
+EOF
+echo '----------------------------------'
+echo 'Add config to .xinitrc'
+echo '----------------------------------'
+echo exec /usr/local/kde4/bin/startkde > ~/.xinitrc
 echo '----------------------------------'
